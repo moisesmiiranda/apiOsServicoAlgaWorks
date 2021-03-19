41ab8ce0f304849a4a2fef31e6196ed03af1d34b
Moisés <moisesmiranda1994@gmail.com>
2021-03-19 08:47:07 -0300

    Primeiro commit

---
+++ src/main/java/com/moises/osservico/PrjOsDeServicoApplication.java
@@ -0,0 +1,13 @@
+package com.moises.osservico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjOsDeServicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjOsDeServicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moises/osservico/PrjOsDeServicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moises.osservico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjOsDeServicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
